e4ee150af567ea2af22692f01f5e0787c6e2ca56
TonyHsu212 <xwm212@outlook.com>
2024-11-30 19:40:14 +1300

    connect to pa mysql

---
+++ connect.py
@@ -0,0 +1,11 @@
+#name: weimin xu
+#studentID: 1159079
+
+# These details are available on the first MySQL Workbench screen
+# Usually called 'Local Instance'
+dbuser = "xwm212" # Your MySQL username - likely 'root'
+# dbpass = "XXXXX" # ---- PUT YOUR PASSWORD HERE ----
+dbpass = "b1j2m3luohong6"
+dbhost = "xwm212.mysql.pythonanywhere-services.com"
+dbport = "server.local_bind_port"
+dbname = "xwm212$usstockpairtradingsystem"
