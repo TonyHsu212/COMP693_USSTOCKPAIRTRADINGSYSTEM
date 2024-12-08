06c7f5e8d03e8666e86bc153a8015e5c3192fff9
TonyHsu212 <xwm212@outlook.com>
2024-12-09 11:39:34 +1300

    update coding

--- connect.py
+++ connect.py
@@ -1,11 +1,21 @@
 #name: weimin xu
 #studentID: 1159079
 
-# These details are available on the first MySQL Workbench screen
-# Usually called 'Local Instance'
-dbuser = "xwm212" # Your MySQL username - likely 'root'
-# dbpass = "XXXXX" # ---- PUT YOUR PASSWORD HERE ----
-dbpass = "b1j2m3luohong6"
-dbhost = "xwm212.mysql.pythonanywhere-services.com"
-dbport = "server.local_bind_port"
-dbname = "xwm212$usstockpairtradingsystem"
+
+########################### for pa environment ########################################
+#---- PUT YOUR username HERE ----
+# dbuser = "xwm212"
+# #---- PUT YOUR PASSWORD HERE ----
+# dbpass = "801221789801"
+# dbhost = "xwm212.mysql.pythonanywhere-services.com"
+# dbport = "server.local_bind_port"
+# dbname = "xwm212$usstockpairtradingsystem"
+
+########################### for local environment ########################################
+# ---- PUT YOUR username HERE ----
+dbuser = "root"
+# ---- PUT YOUR PASSWORD HERE ----
+dbpass = "801221789801"
+dbhost = "localhost"
+dbport = "3306"
+dbname = "tradingsystem"
