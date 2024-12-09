91fc9a9986cd0980d5411f023928bb6ffc3a9bd6
TonyHsu212 <xwm212@outlook.com>
2024-12-10 09:40:30 +1300

    improving codes

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import os
-
 from flask import Flask, render_template
-import mysql.connector
-from flask_hashing import Hashing
-
-import connect
 import globalvaluemanagement
 from accountManagement import auth_bp
 
@@ -13,7 +8,7 @@ app = Flask(__name__)
 dbconn = None
 connection = None
 app.secret_key = os.urandom(24)
-UploadDir = '/static/images/'
+# UploadDir = '/static/images/'
 login_num = 0
 globalvaluemanagement._init()
 globalvaluemanagement.set_value('0', 0)
@@ -25,11 +20,6 @@ app.register_blueprint(auth_bp, url_prefix='/auth')
 @app.route("/")
 def dashboard():
     return render_template("home.html")
-
-
-# @app.route("/")
-# def dashboard():
-#     return render_template("dashboard.html")
 
 
 if __name__ == "__main__":
