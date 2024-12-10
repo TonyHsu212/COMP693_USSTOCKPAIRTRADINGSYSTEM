aaaf1a7342dba3a36304c1a51dcc3e5be3c94402
TonyHsu212 <xwm212@outlook.com>
2024-12-11 10:27:28 +1300

    static file

---
+++ static/js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("darkModeSwitch").addEventListener("change", function () {
+  document.body.classList.toggle("bg-dark");
+  document.body.classList.toggle("text-light");
+});
